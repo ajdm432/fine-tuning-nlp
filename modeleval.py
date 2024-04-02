261a68eaff22d2d064a7a778f1ebae170dd1b48e
ama301 <alexander_mountain@sfu.ca>
2024-04-02 09:22:06 -0700

    F18

--- modeleval.py
+++ modeleval.py
@@ -5,7 +5,7 @@ import torch
 METRIC = "rouge"
 NUM_EXAMPLES = 3
 MAX_SEQ_LENGTH = 4096
-MAX_OUT_LENGTH = 20
+MAX_OUT_LENGTH = 800
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 metric = evaluate.load(METRIC)
